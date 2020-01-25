19c77a90c18eb32678ad3e6a343b7a2409304144
Nicholas Stavinski <nstavinski22@verizon.net>
2020-01-25 15:21:58 -0500

    add docstring to open_pdb function

--- molecool/io/pdb.py
+++ molecool/io/pdb.py
@@ -1,17 +1,45 @@
+"""
+Functions for manipulating pdb files
+
+"""
+
 import numpy as np
 
 
-def open_pdb(f_loc):
+def open_pdb(file_location):
+    """
+    Open and read coordinates and symbols from a pdb file.
+
+    The pdb file must specify the atom elements in the last
+    column, and follow the conventions outlined in the PDB format specification.
+
+    Parameters
+    ----------
+    file_location : str
+        The location of the pdb file to read
+        in
+
+    Returns
+    -------
+    coords : np.ndarray
+        The coordinates from the pdb file.
+    symbols : list
+        The atomic symbols from the pdb file.
+    """
     # This function reads in a pdb file and returns the atom names and coordinates.
-    with open(f_loc) as f:
+    with open(file_location) as f:
         data = f.readlines()
-    c = []
-    sym = []
-    for l in data:
-        if 'ATOM' in l[0:6] or 'HETATM' in l[0:6]:
-            sym.append(l[76:79].strip())
-            c2 = [float(x) for x in l[30:55].split()]
-            c.append(c2)
-    coords = np.array(c)
-    return sym, coords
+
+    coordinates = []
+    symbols = []
+    for line in data:
+        if 'ATOM' in line[0:6] or 'HETATM' in line[0:6]:
+            symbols.append(line[76:79].strip())
+            atom_coordinates = [float(x) for x in line[30:55].split()]
+            coordinates.append(atom_coordinates)
+
+    # Convert list to numpy array
+    coordinates = np.array(coordinates)
+    
+    return symbols, coordinates
 
